fee8d4a5815ffc1cbdeeaa34ff53af700f0d8b3f
Juan David Lopez <david16115@outlook.com>
2022-09-29 20:25:05 -0300

    Primmer commit

---
+++ nivel1.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <?php
+    require "phpCss/nivel1php.php";
+    ?>
+
+</head>
+<body>
+    
+    <table>
+        <tr>
+            <td> <input min=1 max=9 type="number" name="" id=""> </td>
+            <td> <input min=1 max=9 type="number" name="" id=""> </td>
+            <td> <input min=1 max=9 type="number" name="" id=""> </td>
+            <td> <input min=1 max=9 type="number" name="" id=""> </td>
+            <td> <input min=1 max=9 type="number" name="" id=""> </td>
+        </tr>
+
+
+        <tr>
+            <td> <input min=1 max=9 type="number" name="" id=""> </td>
+            <td> <input min=1 max=9 type="number" name="" id=""> </td>
+            <td> <input min=1 max=9 type="number" name="" id=""> </td>
+            <td> <input min=1 max=9 type="number" name="" id=""> </td>
+            <td> <input min=1 max=9 type="number" name="" id=""> </td>
+        </tr>
+
+
+        <tr>
+            <td> <input min=1 max=9 type="number" name="" id=""> </td>
+            <td> <input min=1 max=9 type="number" name="" id=""> </td>
+            <td> <input min=1 max=9 type="number" name="" id=""> </td>
+            <td> <input min=1 max=9 type="number" name="" id=""> </td>
+            <td> <input min=1 max=9 type="number" name="" id=""> </td>
+        </tr>
+
+        <tr>
+            <td> <input min=1 max=9 type="number" name="" id=""> </td>
+            <td> <input min=1 max=9 type="number" name="" id=""> </td>
+            <td> <input min=1 max=9 type="number" name="" id=""> </td>
+            <td> <input min=1 max=9 type="number" name="" id=""> </td>
+            <td> <input min=1 max=9 type="number" name="" id=""> </td>
+        </tr>
+
+        <tr>
+            <td> <input min=1 max=9 type="number" name="" id=""> </td>
+            <td> <input min=1 max=9 type="number" name="" id=""> </td>
+            <td> <input min=1 max=9 type="number" name="" id=""> </td>
+            <td> <input min=1 max=9 type="number" name="" id=""> </td>
+            <td> <input min=1 max=9 type="number" name="" id=""> </td>
+        </tr>
+
+    
+    </table>
+</body>
+</html>
---
+++ phpCss/nivel1php.php
@@ -0,0 +1,22 @@
+<title>Sudoku</title>
+
+<style>
+:root{
+    --white:white;
+    --c1:thistle;
+    --c2:plum;
+    --c3:violet;  
+    --c4:orchid;
+    --c5:magenta;
+    --c6:mediumOrchid;
+}
+
+table,td{
+    border:1px solid black;
+}
+
+body{
+    background-color: var(--white);
+}
+
+</style>
