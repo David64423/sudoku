27dae43daab49355ddcca37ba62fe5d14937e034
Matias <matiaskarlen0678@gmail.com>
2022-10-02 01:35:14 -0300

    creo el login simple y la pag de niveles

---
+++ login.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>login simple</title>
+    <link rel="stylesheet" href="css/login.css">
+</head>
+<body>
+    <section >
+    <div class="login">
+        <h1>¡Bienvenido!</h1>
+        <br><p>Por favor registrese para ingresar</p><br>
+        <form >
+            <div id="aa">Nombre de usuario:<br><input id="input" type="text" name="usuario"></div><br><br>
+            <div id="aa">Contraseña:<br><input id="input" type="text" name="contra"></div><br>
+            <ul><input id="iniciar" type="submit" value="Ingresar"></ul>    
+        </form>
+    </div>
+    </section>
+    <div id="finalpag">Todos los derechos reservados 2022-<?php echo date("Y");?></div>
+
+</body>
+</html>
---
+++ niveles.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/niveles.css">
+</head>
+<body>
+<h1>Juego: sudoku</h1>
+<h1>Seleccion de niveles</h1>
+    <section class="flex-container">
+        <div class="caja">
+            <a href="">Nivel 1</a>
+        </div>
+        <div class="caja">
+            <a href="">Nivel 2</a>
+        </div>
+        <div class="caja">
+            <a href="">Nivel 3</a>
+        </div>
+       
+    </section>
+</body>
+</html>
