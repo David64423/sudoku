45060e6ceb8eb3913a63681e48738ea65d1bc374
Dana More <danamore@live.com>
2022-11-01 15:13:50 -0300

    valores diagonal principal precargados del nivel 7x7

--- js/nivel2.js
+++ js/nivel2.js
@@ -2,5 +2,32 @@ import {verificar} from './sudoku.js';
 import {main} from './sudoku.js';
 import {funcionEstrellas} from './sudoku.js';
 
-main("31242656524536322531");
+let lugares = document.getElementsByClassName("lugares");
+
+let lugaresRellenar = [0,7,14,21,28,35];
+
+let camino=Math.floor(Math.random()*3);
+
+if(camino==0){
+
+    main("531264246153163542452316314625265431");
+    rellenarDiagonal(lugares,[5,4,3,3,2,1],lugaresRellenar);
+
+}
+
+else if(camino==1){
+    main("142563536241315624624315463152251436");
+    rellenarDiagonal(lugares,[1,3,5,3,5,6],lugaresRellenar);
+
+}
+
+else{
+    main("456231312645164352523164235416641523");
+    rellenarDiagonal(lugares,[4,1,4,1,1,3],lugaresRellenar);
+}
+
+let a=[0,1,2,3];
+a.length;
+
+
 funcionEstrellas();
